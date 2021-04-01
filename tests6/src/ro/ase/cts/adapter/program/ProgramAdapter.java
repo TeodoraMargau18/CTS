5bead2c68566147cbcd511a6742e2675a146e498
TeodoraMargau18 <margauteodora18@stud.ase.ro>
2021-04-01 13:17:43 +0300

    second commit s6

---
+++ tests6/src/ro/ase/cts/adapter/program/ProgramAdapter.java
@@ -0,0 +1,27 @@
+package ro.ase.cts.adapter.program;
+
+import ro.ase.cts.adapter.clase.Bilet;
+import ro.ase.cts.adapter.clase.BiletAdapter;
+import ro.ase.cts.adapter.clase.BiletOnline;
+
+public class ProgramAdapter {
+	
+	public static void rezeervaSiVindeBiletOnline(BiletOnline biletOnline) {
+		biletOnline.rezervaBiletOnline();
+		biletOnline.vindeBiletOnline();
+	}
+	public static void rezeervaSiVindeBiletCasa(Bilet bilet) {
+		bilet.rezerva();
+		bilet.vinde();
+	}
+
+	public static void main(String[] args) {
+
+		BiletOnline biletOnline=new BiletAdapter(12);
+		Bilet bilet=new BiletAdapter(22);
+		rezeervaSiVindeBiletOnline(biletOnline);
+		rezeervaSiVindeBiletCasa(bilet);
+		
+	}
+
+}
